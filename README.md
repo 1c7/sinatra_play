6980146c2376d201ea31eacab0bef39af6136cfa
1c7 <guokrfans@gmail.com>
2016-12-17 00:03:33 +0800

    enough

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# sinatra_play
+# Play with Sinatra
+http://www.sinatrarb.com
+
+
+1. public/ is default dir to serve static file
+1. views/ is default dir for put view template
+1. must restart server to pick up file change.
--- hello.rb
+++ hello.rb
@@ -7,3 +7,17 @@ end
 get '/frank-says' do
    'Put this in your pipe & smoke it!'
 end
+
+post '/' do
+
+end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
+
+get '/s' do
+  erb :index
+end
